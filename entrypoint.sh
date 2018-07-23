51e5e7885a5bc4af5584c5325a30408b386ecf4e
Ravinou <39600829+Ravinou@users.noreply.github.com>
2018-07-23 22:31:50 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+#Si execution en tant que user root, le script doit l'être en tant que user cod4server, donc remplacez par : su - cod4server -s /bin/bash -c "/home/cod4server/cod4server start
+
 set -e
 
 if [ "$1" = 'start' ]; then
-	su - cod4server -s /bin/bash -c "/home/cod4server/cod4server start"
+	/home/cod4server/cod4server start
 fi
